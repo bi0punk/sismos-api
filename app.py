52192bf83459dc46ce2d826d27afada62a7fc850
sysbot <systecinfo_cl@criptext.com>
2022-11-07 02:42:32 -0300

    0242

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, jsonify
+from bs4 import BeautifulSoup
+import urllib.request
+import pandas as pd
+import requests
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['GET', 'POST'])
+def hello_world():
+    table_MN = pd.read_html('https://www.sismologia.cl/sismicidad/catalogo/2022/11/20221107.html')
+    print(f'Total tables: {len(table_MN)}')
+    df = table_MN[1]
+
+    data = df.to_json(orient='index')
+    """ return (data) """
+
+    return (data)
+    
+    """ return jsonify(df) """
+
+
+
+
