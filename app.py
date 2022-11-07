ba1c00bd9c05d8ed0079bb835503308f12aa4227
sysbot <systecinfo_cl@criptext.com>
2022-11-07 03:07:18 -0300

    fix0307

--- app.py
+++ app.py
@@ -4,19 +4,17 @@ import urllib.request
 import pandas as pd
 import requests
 
-app = Flask(__name__)
+application = Flask(__name__)
+app = application
 
-@app.route("/")
+@application.route("/")
 def hello_world():
     table_MN = pd.read_html('https://www.sismologia.cl/sismicidad/catalogo/2022/11/20221107.html')
     print(f'Total tables: {len(table_MN)}')
     df = table_MN[1]
-
     data = df.to_json(orient='index')
     """ return (data) """
-
     return (data)
-    
     """ return jsonify(df) """
 
 
